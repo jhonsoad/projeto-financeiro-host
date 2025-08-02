76191eb9acca4bfa73fadf41959d9106c902fe38
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2025-08-02 11:23:35 -0300

    cria projeto host para mfe com native-federation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: '', 
+    component: HomeComponent
+  },
+  { 
+    path: 'projetoFinanceiro', 
+    loadComponent: () => loadRemoteModule('mfe1', './Component')
+      .then((m) => m.HomeComponent)
+  }
+
+];
